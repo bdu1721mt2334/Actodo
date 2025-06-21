c61fe805c98532bea99bbe3bd8ccee77e202b388
bdu1721mt2334 <sujan93600@gmail.com>
2025-06-21 21:24:36 +0430

    Updated Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -11,7 +11,7 @@ function Landing()
     return(
         <div className='bg-black p-16'>
       <div className='bg-white p-10 border rounded-md'>
-        <Header  name={data.store.user}></Header>
+        <Header  ></Header>
 
 
         <div className='flex justify-between gap-7 my-5 flex-wrap'>
